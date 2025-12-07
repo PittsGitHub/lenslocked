713656f74435df716c6e596af3d1fce3ff466046
Daniel Pittard <PittsGitHub>
2025-12-07 17:26:19 +0000

    FAQ Task

--- main.go
+++ main.go
@@ -9,6 +9,37 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `
+		<h1>FAQ</h1>
+		<p>Frequently asked questions about Bilbo.</p>
+		<p/>
+		<ul>
+			<li>
+				<strong>Who is Bilbo?</strong><br>
+				Bilbo is a small scruffy black haired jack-a-poo with a knack for getting into (and out of) trouble.
+			</li>
+			<p/>
+			<li>
+				<strong>What does Bilbo do all day?</strong><br>
+				Mostly minds his own business, snacks frequently, and occasionally goes on unexpected walks.
+			</li>
+			<p/>
+			<li>
+				<strong>Does Bilbo like visitors?</strong><br>
+				Yes. Other dogs. People. Even cats.
+			</li>
+			<p/>
+			<li>
+				<strong>How can I learn more about Bilbo?</strong><br>
+				You can email Bilbo at 
+				<a href="mailto:bilbo@borks.alot">bilbo@borks.alot</a>.
+			</li>
+		</ul>
+	`)
+}
+
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, `<h1>Contact Page</h1><p>To get in touch email me at
@@ -28,6 +59,8 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	case "/":
 		homeHandler(w, r)
 	default:
