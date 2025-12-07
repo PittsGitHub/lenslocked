91775270ed826cd894ea1502b0793dd3d2517b64
Daniel Pittard <PittsGitHub>
2025-12-07 15:38:02 +0000

    Scaffolded Page -contact -home -custom 404 correct status in response

--- main.go
+++ main.go
@@ -5,17 +5,36 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
-func handlerFunc2(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my big air site!</h1>")
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>Contact Page</h1><p>To get in touch email me at
+	<a href="mailto:frontrowpittard@gmail.com">frontrowpittard@gmail.com</a>.`)
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, `<p>Open the door get on the floor, baby it's a </p>`)
+	fmt.Fprint(w, `<h1>404 <span style="font-size: 0.5em;">not found</span></h1>`)
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/contact":
+		contactHandler(w, r)
+	case "/":
+		homeHandler(w, r)
+	default:
+		notFoundHandler(w, r)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.HandleFunc("/big", handlerFunc2)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Starting server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
