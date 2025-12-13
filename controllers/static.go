7796ececed77c67403cb3842c5310af392c19f92
Daniel Pittard <PittsGitHub>
2025-12-13 16:01:11 +0000

    .gohtml dev env supp

--- controllers/static.go
+++ controllers/static.go
@@ -11,3 +11,31 @@ func StaticHandler(tpl views.Template) http.HandlerFunc {
 		tpl.Execute(w, nil)
 	}
 }
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   string
+	}{
+		{
+			Question: "Who is Bilbo?",
+			Answer:   "Bilbo is a small scruffy black haired jack-a-poo with a knack for getting into (and out of) trouble.",
+		},
+		{
+			Question: "What does Bilbo do all day?",
+			Answer:   "Mostly minds his own business, snacks frequently, and occasionally goes on unexpected walks.",
+		},
+		{
+			Question: "Does Bilbo like visitors?",
+			Answer:   "Yes. Other dogs. People. Even cats.",
+		},
+		{
+			Question: "How can I learn more about Bilbo?",
+			Answer:   `You can email Bilbo at <a href="mailto:bilbo@borks.alot">bilbo@borks.alot</a>.`,
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
+	}
+
+}
--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 	r.Get("/contact", controllers.StaticHandler(views.Must(
 		views.ParseFS(templates.FS, "contact.gohtml"))))
 
-	r.Get("/faq", controllers.StaticHandler(views.Must(
+	r.Get("/faq", controllers.FAQ(views.Must(
 		views.ParseFS(templates.FS, "faq.gohtml"))))
 
 	r.Get("/owner", controllers.StaticHandler(views.Must(
