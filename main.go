d69485d8ab7a190a0924339a8af21a4fdff3eb96
Daniel Pittard <PittsGitHub>
2025-12-14 13:24:49 +0000

    css

--- main.go
+++ main.go
@@ -25,19 +25,19 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Get("/", controllers.StaticHandler(views.Must(
-		views.ParseFS(templates.FS, "home.gohtml", "layout-parts.gohtml"))))
+		views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
 
 	r.Get("/contact", controllers.StaticHandler(views.Must(
-		views.ParseFS(templates.FS, "contact.gohtml"))))
+		views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
 
 	r.Get("/faq", controllers.FAQ(views.Must(
-		views.ParseFS(templates.FS, "faq.gohtml"))))
+		views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
 
 	r.Get("/owner", controllers.StaticHandler(views.Must(
-		views.ParseFS(templates.FS, "owner.gohtml"))))
+		views.ParseFS(templates.FS, "owner.gohtml", "tailwind.gohtml"))))
 
 	r.NotFound(controllers.StaticHandler(views.Must(
-		views.ParseFS(templates.FS, "notfound.gohtml"))))
+		views.ParseFS(templates.FS, "notfound.gohtml", "tailwind.gohtml"))))
 
 	fmt.Println("Starting server on :3000...")
 	http.ListenAndServe(":3000", r)
